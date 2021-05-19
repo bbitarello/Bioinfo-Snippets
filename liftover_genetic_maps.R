5ac5d405562a2819a262e8a18116648a5e313769
Bárbara Bitarello <barbara.bitarello@pennmedicine.upenn.edu>
2021-05-18 20:49:51 -0400

    Update liftover_genetic_maps.R

--- liftover_genetic_maps.R
+++ liftover_genetic_maps.R
@@ -1,13 +1,16 @@
-library(bigsnpr)
-library(bigstatsr)
-library(dplyr)
-library(bigreadr)
-library(data.table)
+liftover_genetic_map<-function(dir="~data/1kg/20130502_phase3_final/", outdir="maps_hg38/", from="hg19", to="hg38", base=".OMNI.interpolated_genetic_map"){
+require(bigsnpr)
+require(bigstatsr)
+require(dplyr)
+require(bigreadr)
+require(data.table)
+assertdir("maps/")
 for(chr in 1:22){
-basename <- paste0("chr", chr, ".OMNI.interpolated_genetic_map")
-info_snp<-fread(glue::glue("/project/mathilab/data/1kg/20130502_phase3_final/{basename}")) #path where hg19 map files can be foun
+basename <- glue::glue("chr{chr}{base}")
+info_snp<-fread(glue::glue("{dir}/{basename}")) #path where hg19 map files can be foun
 setnames(info_snp, c('SNP', 'pos', 'Dist'))
 info_snp[, chr:=chr]
-snp_modifyBuild(info_snp, from="hg19", to="hg38", liftOver="/project/mathilab/bin/liftOver")[,.(SNP, pos, Dist)] %>% fwrite2(file=glue::glue("maps_hg38/chr{chr}.OMNI.interpolated_genetic_map"), col.names=F, sep="\t")
+snp_modifyBuild(info_snp, from="hg19", to="hg38", liftOver="~/bin/liftOver")[,.(SNP, pos, Dist)] %>% fwrite2(file=glue::glue("{outdir}{basename}"), col.names=F, sep="\t")
 cat(chr, '\n')
+}
 }
